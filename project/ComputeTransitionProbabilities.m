7ce5a15fcb01dbb149a0e31cca3b22544e516e4d
Doris Fonseca Lima <donseca@student-net-vpn-3437.ethz.ch>
2019-12-02 17:49:17 +0100

    Small changes added to caroline's function, new function added for Prob(crash|wind): the crash due to tree and due to boundary knowing that there is wind are not independent...

--- project/ComputeTransitionProbabilities.m
+++ project/ComputeTransitionProbabilities.m
@@ -28,11 +28,26 @@ global GAMMA R P_WIND
 global FREE TREE SHOOTER PICK_UP DROP_OFF BASE
 global NORTH SOUTH EAST WEST HOVER
 global K TERMINAL_STATE_INDEX
-P = zeros(K, K, 3);
+P = zeros(K, K, 5);
 for u = [NORTH, SOUTH, EAST, WEST, HOVER]
-    for i = size(stateSpace, 1)
-        %if !BoundsCheck(state, u, map) || TreesCheck(state, u, map)                
-        for j = 
+    for i = 1:1:size(stateSpace,1)
+        carry_state = stateSpace(i,3);
+        stateSpace_red = stateSpace(stateSpace(:,3)==carry_state,:);
+        %the stateSpace could be further reduced by introducing a filter
+        %ofr the 13 attainable states
+        %drone_range =
+        if BoundariesCheck(i, u, stateSpace_red) || ~TreesCheck(i, u, map,stateSpace_red) 
+            j = stateSpace(i,2);
+            switch u
+                case NORTH
+                    %Probabilities with wind
+                    P_Crash=                    
+                    P(i,j,u) = P_WIND*(1/4)*(1-P_Tree)*(1-P_Bound)*(1-ComputeAngryResidentsCrashProbability(stateSpace(j,:), map))
+                    P(i+2,j,u) = 
+                    P(i+1,j-1,u) = 
+                    p(i+1,j+1,u) = 
+                    %Probabilities without wind
+        end
     end
 end
 
@@ -55,7 +70,7 @@ function trees = TreesCheck(i, u, map,stateSpace)
 %
 %Output : true or false
 global TREE
-global NORTH SOUTH EAST WEST
+global NORTH SOUTH EAST WEST 
 pos =[];
 cdt = [];
 pos = stateSpace(i,1:2); %actual position of the drone
@@ -103,7 +118,7 @@ function boundaries = BoundariesCheck(i, u, stateSpace)
 %
 %
 %Output : true or false
-global NORTH SOUTH EAST WEST
+global NORTH SOUTH EAST WEST 
 pos =[];
 cdt = [];
 %looking for the position of the drone
@@ -121,15 +136,16 @@ cdt = [isequal(ismember(boundMNorth,pos,'rows'), zeros(length(boundMNorth),1));
     isequal(ismember(boundNEast,pos,'rows'), zeros(length(boundNEast),1));
     isequal(ismember(boundNWest,pos,'rows'), zeros(length(boundNWest),1));
     isequal(ismember(boundCorners,pos,'rows'), zeros(length(boundCorners),1))];
-
+disp(cdt)
 %still looking where the drone is and if the input is possible
+boundaries = 0;
 if cdt(1) == 0 
     bound = boundMNorth;
     fprintf('Drone in north bound');
     if u == NORTH
             fprintf('Drone outside boundaries, crashes');
             boundaries = true;
-            return
+            return 
     end   
 elseif cdt(2) == 0
         bound = boundMSouth;
@@ -161,16 +177,16 @@ elseif cdt(4) == 0
 elseif cdt(5) == 0
         bound = boundCorners;
         fprintf('Drone in a corner');
-        corner = ismember(boundCorners,pos,'rows')
-        if corner(1) == 1 && (u == WEST || u == HOVER)
+        corner = ismember(boundCorners,pos,'rows');
+        if corner(1) == 1 && (u == WEST || u == SOUTH)
             fprintf('Drone outside boundaries, crashes');
             boundaries = true;
-            return
+            return 
         elseif corner(2) == 1 && (u == NORTH || u == WEST)
             fprintf('Drone outside boundaries, crashes');
             boundaries = true;
             return
-        elseif corner(3) == 1 && (u == HOVER || u == EAST)
+        elseif corner(3) == 1 && (u == SOUTH || u == EAST)
             fprintf('Drone outside boundaries, crashes');
             boundaries = true;
             return
@@ -179,10 +195,13 @@ elseif cdt(5) == 0
             boundaries = true;
             return
         end
-else 
+end
+if boundaries ==0
     fprintf('Drone OK');
     boundaries = false;
+    return
 end
+
 
 end
 
@@ -202,3 +221,9 @@ d = vecnorm((repmat(currentState(1:2), size(shootersCoordinates, 1), 1) -...
     shootersCoordinates).', 1).';
 Pr =  (d <= R).' * (GAMMA ./ (d + 1));
 end
+
+function P_Tree = ComputeCrashProbabilityWind(currentState,map)
+%Function that computes the probability of crashing due to a tree or the boundary in one of
+%the states attainable by wind
+end
+
